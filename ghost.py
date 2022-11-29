80456f1018b16f2ca86c56868ecbec8fe2e2024c
kortiz21 <105018036+kortiz21@users.noreply.github.com>
2022-11-29 10:56:26 -0800

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -15,7 +15,7 @@ class Ghost(Sprite):
         self.maze = self.game.maze.currMaze
         self.barriers = self.game.maze.barriers
 
-        self.img = pg.image.load('images/pacman_0.png')
+        # self.img = pg.image.load('images/pacman_0.png')
         self.rect = self.img.get_rect()
         self.pos = Vec(location[0], location[1])
         self.dir = 'STOP'
@@ -26,24 +26,6 @@ class Ghost(Sprite):
         self.last = pg.time.get_ticks()
         self.delay = 200
         self.scores = GameController()
-
-        pacman_frames_r = [pg.image.load(
-            f'images/pacman_{i}.png') for i in range(3)]
-        pacman_frames_l = [pg.transform.flip(pg.image.load(
-            f'images/pacman_{i}.png'), flip_x=True, flip_y=False) for i in range(3)]
-        pacman_frames_u = [pg.transform.rotate(pg.image.load(
-            f'images/pacman_{i}.png'), 90) for i in range(3)]
-        pacman_frames_d = [pg.transform.rotate(pg.image.load(
-            f'images/pacman_{i}.png'), -90) for i in range(3)]
-
-        timer_left = Timer(frames=pacman_frames_l, looponce=False, wait=250)
-        timer_right = Timer(frames=pacman_frames_r, looponce=False, wait=250)
-        timer_up = Timer(frames=pacman_frames_u, looponce=False, wait=250)
-        timer_down = Timer(frames=pacman_frames_d, looponce=False, wait=250)
-
-        self.timers = {'L': timer_left, 'R': timer_right,
-                       'U': timer_up, 'D': timer_down}
-        self.timer = self.timers['L']
 
 
 
