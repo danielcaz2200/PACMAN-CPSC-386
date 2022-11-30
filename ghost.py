461344e013caa54a1441e48010fdcc94e84f9a39
kortiz21 <105018036+kortiz21@users.noreply.github.com>
2022-11-30 13:24:55 -0800

    Update ghost.py
    updated ghosts.py, added all four ghost

--- ghost.py
+++ ghost.py
@@ -6,32 +6,40 @@ from timer import Timer
 from menus import GameController
 
 
-class Ghosts():
-    def __init__(self, game, location: tuple) -> None:
+class Ghost(Sprite):
+    def __init__(self, game, x, y):
         super().__init__()
-        self.game = game
-        self.locations = self.game.locations
-        self.settings = game.settings
-        self.maze = self.game.maze.currMaze
-        self.barriers = self.game.maze.barriers
-
-        # self.img = pg.image.load('images/pacman_0.png')
-        self.rect = self.img.get_rect()
-        self.pos = Vec(location[0], location[1])
-        self.dir = 'STOP'
         self.screen = game.screen
-        self.speed = self.settings.playerSpeed
-        self.rect.left = self.pos.x * self.rect.width
-        self.rect.top = self.pos.y * self.rect.height
-        self.last = pg.time.get_ticks()
-        self.delay = 200
-        self.scores = GameController()
-
-
-
-    def checkCollisions(self): pass
-
-    def update(self): pass
+        self.img = pg.image.load('images/blinky_0.png')
+        self.rect = self.img.get_rect()
+        self.rect.left, self.rect.top = x * self.rect.width, y * self.rect.height
 
     def draw(self):
-        self.screen.blit(self.timer.imagerect(), self.rect)
+        self.screen.blit(self.img, self.rect)
+
+    def update(self):
+        self.draw()
+
+
+class Blinky(Ghost):
+    def __init__(self, game, x, y):
+        Ghost.__init__(self, game, x, y)
+        self.img = pg.image.load('images/blinky_0.png')
+
+
+class Pinky(Ghost):
+    def __init__(self, game, x, y):
+        Ghost.__init__(self, game, x, y)
+        self.img = pg.image.load('images/pinky_0.png')
+
+
+class Inky(Ghost):
+    def __init__(self, game, x, y):
+        Ghost.__init__(self, game, x, y)
+        self.img = pg.image.load('images/inky_0.png')
+
+
+class Clyde(Ghost):
+    def __init__(self, game, x, y):
+        Ghost.__init__(self, game, x, y)
+        self.img = pg.image.load('images/clyde_0.png')
